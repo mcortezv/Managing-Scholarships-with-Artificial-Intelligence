cdac9d9ee34de3851b615a17e8bafe947ad156b1
SebastianEsc05 <sebastianescram01@gmail.com>
2025-12-08 16:35:18 -0700

    fix dtoi && dto

--- sistema_escolar/negocios_solicitarPago/src/main/java/adaptadoresPagoAdeudo/PrestamoAdaptador.java
+++ sistema_escolar/negocios_solicitarPago/src/main/java/adaptadoresPagoAdeudo/PrestamoAdaptador.java
@@ -6,44 +6,8 @@ import dto.pagarAdeudo.PrestamoDTO;
 
 public class PrestamoAdaptador {
 
-    public static PrestamoDTO toDTO(PrestamoDTOI dtoi) {
-        if (dtoi == null) {
-            return null;
-        }
-        PrestamoDTO dto = new PrestamoDTO();
-        dto.setCosto(dtoi.getCosto());
-        dto.setFechaPrestamo(dtoi.getFechaPrestamo());
-        dto.setFechaDevolucion(dtoi.getFechaDevolucion());
-        dto.setIsbn(dtoi.getIsbn());
-        dto.setTitulo(dtoi.getTitulo());
-        dto.setCampus(dtoi.getCampus());
-        dto.setDetalles(dtoi.getDetalles());
-        dto.setEstatus(dtoi.getEstatus());
-        dto.setIdEstudiante(dtoi.getIdEstudiante());
-        return dto;
-    }
-
-    public static PrestamoDTOI toDTOI(PrestamoDTO dto) {
-        if (dto == null) {
-            return null;
-        }
-        PrestamoDTOI dtoi = new PrestamoDTOI();
-        dtoi.setCosto(dto.getCosto());
-        dtoi.setFechaPrestamo(dto.getFechaPrestamo());
-        dtoi.setFechaDevolucion(dto.getFechaDevolucion());
-        dtoi.setIsbn(dto.getIsbn());
-        dtoi.setTitulo(dto.getTitulo());
-        dtoi.setCampus(dto.getCampus());
-        dtoi.setDetalles(dto.getDetalles());
-        dtoi.setEstatus(dto.getEstatus());
-        dtoi.setIdEstudiante(dto.getIdEstudiante());
-        return dtoi;
-    }
-
-
     public static PrestamoDTO toDTO(Prestamo entity) {
         if (entity == null) return null;
-
         PrestamoDTO dto = new PrestamoDTO();
         dto.setCosto(entity.getCosto());
         dto.setFechaDevolucion(entity.getFechaDevolucionProgramada());
--- sistema_escolar/negocios_solicitarPago/src/main/java/adaptadoresPagoAdeudo/SolicitudPagoAdapdator.java
+++ sistema_escolar/negocios_solicitarPago/src/main/java/adaptadoresPagoAdeudo/SolicitudPagoAdapdator.java
@@ -1,4 +1,37 @@
 package adaptadoresPagoAdeudo;
 
+import dto.pagarAdeudo.SolicitudPagoDTO;
+import itson.pagarAdeudo.SolicitudPagoDTOI;
+
 public class SolicitudPagoAdapdator {
+    public static SolicitudPagoDTO toDTO(SolicitudPagoDTOI dtoI) {
+        if (dtoI == null) return null;
+
+        SolicitudPagoDTO dto = new SolicitudPagoDTO();
+
+        dto.setIdEstudiante(dtoI.getIdEstudiante());
+        dto.setReferenciaPago(dtoI.getReferenciaPago());
+        dto.setFechaPago(dtoI.getFechaPago());
+        dto.setMontoPagado(dtoI.getMontoPagado());
+        dto.setMetodoPago(dtoI.getMetodoPago());
+        dto.setEstatusPago(dtoI.getEstatusPago());
+        dto.setTipoAdeudo(dtoI.getTipoAdeudo());
+        return dto;
+    }
+
+    public static SolicitudPagoDTOI toDTOI(SolicitudPagoDTO dto) {
+        if (dto == null) return null;
+
+        SolicitudPagoDTOI dtoI = new SolicitudPagoDTOI();
+
+        dtoI.setIdEstudiante(dto.getIdEstudiante());
+        dtoI.setReferenciaPago(dto.getReferenciaPago());
+        dtoI.setFechaPago(dto.getFechaPago());
+        dtoI.setMontoPagado(dto.getMontoPagado());
+        dtoI.setMetodoPago(dto.getMetodoPago());
+        dtoI.setEstatusPago(dto.getEstatusPago());
+        dtoI.setTipoAdeudo(dto.getTipoAdeudo());
+
+        return dtoI;
+    }
 }
