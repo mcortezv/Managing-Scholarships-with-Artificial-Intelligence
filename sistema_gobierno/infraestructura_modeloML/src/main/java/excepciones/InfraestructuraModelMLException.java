0aff451d3f8a94d6441d3d2152572db10aaad39e
mcortezv <mdjesuscv@gmail.com>
2025-12-08 23:56:25 -0700

    Infraestructura Model_ML

--- sistema_gobierno/infraestructura_modeloML/src/main/java/excepciones/InfraestructuraModelMLException.java
+++ sistema_gobierno/infraestructura_modeloML/src/main/java/excepciones/InfraestructuraModelMLException.java
@@ -1,9 +1,15 @@
 package excepciones;
 
 /**
+ * The type Infraestructura model ml exception.
  *
  * @author Cortez, Manuel;
  */
 public class InfraestructuraModelMLException extends RuntimeException {
+    /**
+     * Instantiates a new Infraestructura model ml exception.
+     *
+     * @param msg the msg
+     */
     public InfraestructuraModelMLException(String msg) { super(msg); }
 }
